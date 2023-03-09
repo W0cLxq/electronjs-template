cce6a96841d8abf61a4ed5409eb46419c5ccb86b
Monoster <123750573+W0cLxq@users.noreply.github.com>
2023-03-09 18:35:00 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntragtion: true,
+      contextIsolation: false,
+      webviewTag: true,
+      nativeWindowOpen: true,
+    }
+  })
+
+  win.loadFile('pages/mainpage.html')
+  win.menuBarVisible = false;
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
